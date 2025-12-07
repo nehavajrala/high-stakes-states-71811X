d75922add71fc4b9f422dd85443ae9566cc44a9d
nehavajrala <neha.vajrala@gmail.com>
2025-12-07 18:34:27 -0500

    Update intake.hpp

--- include/systems/intake.hpp
+++ include/systems/intake.hpp
@@ -7,7 +7,7 @@ inline pros::Motor intake1(18);  // Make this number negative if you want to rev
 inline pros::Motor intake2(10);
 inline pros::Optical optical_sensor(9);
 
-
+//void sets intake at the input so basically add void 
 void set_intakeRED(int input);
 void REDintake_opcontrol();
 void set_intakeBLUE(int input);
@@ -16,7 +16,7 @@ void block_intake_extras();
 void continue_intake_extras();
 void set_intake_blank(int input);
 void BLANKintake_opcontrol();
-
+//bottom, middle, top. 
 void Intake1Control(int input);
 void Intake2Control(int input);
 
@@ -28,3 +28,5 @@ void AutonIntakeLoopBlue();
 
 void DisableAutonLoops();
 void ContinueAutonLoops();
+
+//
